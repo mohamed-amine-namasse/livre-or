1606ddc174869ee3db35b67ae30a7750e158c0e3
MedAmine36 <mohamed-amine.namasse@laplateforme.io>
2025-08-10 14:07:45 +0200

    travail ok

---
+++ connexion.php
@@ -0,0 +1,245 @@
+<?php
+      
+      
+        
+        $message = '';      // Message à afficher à l'utilisateur
+        
+        /*****************************************
+         *  Vérification du formulaire connexion
+        *****************************************/
+        // Si le tableau $_POST existe alors le formulaire a été envoyé
+        if(!empty($_POST))
+        {
+            
+            // Le formulaire a été envoyé vide
+            if(empty($_POST['login'])||empty($_POST['password'])||empty($_POST['confirm_password']))
+            {
+            $message = 'Des champs vides dans le formulaire ont été envoyés ! Veuillez entrer des champs valides';
+            }
+            if($_POST["password"]!=$_POST["confirm_password"]) {
+            $message = 'les deux mots de passe ne sont pas identiques! Veuillez réessayer';}
+            
+        }
+
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page de connexion</title>
+    <link rel="stylesheet" href="./assets/css/style.css" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@100..900&display=swap" rel="stylesheet">
+
+
+    <!-- Load an icon library -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+</head>
+
+<body>
+
+    <header>
+
+
+        <ul>
+            <li>
+                <a href="./index.php" class="#">
+                    <div class="icon">
+                        <i class="fa fa-home"></i>
+                        <i class="fa fa-home"></i>
+                    </div>
+                    <div class="name">Home</div>
+                </a>
+            </li>
+            <?php if (!isset($_SESSION['login'])): ?>
+
+            <li>
+                <a href="./connexion.php" class="active">
+                    <div class="icon">
+                        <i class="fa fa-user-circle-o"></i>
+                        <i class="fa fa-user-circle-o"></i>
+                    </div>
+                    <div class="name">Se connecter</div>
+                </a>
+            </li>
+            <li>
+                <a href="./inscription.php" class="#">
+                    <div class="icon">
+                        <i class="fa fa-user-plus"></i>
+                        <i class="fa fa-user-plus"></i>
+                    </div>
+                    <div class="name">Inscription</div>
+                </a>
+            </li>
+            <li>
+                <a href="./livreor.php" class="#">
+                    <div class="icon">
+                        <i class="fa fa-book"></i>
+                        <i class="fa fa-book"></i>
+                    </div>
+                    <div class="name">Livre d'or</div>
+                </a>
+            </li>
+            <?php else: ?>
+
+            <li>
+                <a href="./profil.php" class="active">
+                    <div class="icon">
+                        <i class="fa fa-user"></i>
+                        <i class="fa fa-user"></i>
+                    </div>
+                    <div class="name">Profil</div>
+                </a>
+            </li>
+            <li>
+                <a href="./commentaire.php" class="#">
+                    <div class="icon">
+                        <i class="fa fa-edit"></i>
+                        <i class="fa fa-edit"></i>
+                    </div>
+                    <div class="name">Commentaires</div>
+                </a>
+            </li>
+            <li>
+                <a href="./deconnexion.php" class="#">
+                    <div class="icon">
+                        <i class="fa fa-sign-out"></i>
+                        <i class="fa fa-sign-out"></i>
+                    </div>
+                    <div class="name">Deconnexion</div>
+                </a>
+            </li>
+            <?php endif; ?>
+        </ul>
+
+
+
+
+    </header>
+
+
+
+    <main>
+        <?php if(!empty($message)) : ?>
+        <p><?php echo $message; ?></p>
+        <?php endif; ?>
+
+        <?php
+        
+        
+        //on établit la connexion avec la base de donnée moduleconnexion
+        $connexion = mysqli_connect('localhost', 'root');
+        mysqli_select_db($connexion, 'livreor'); 
+        /*
+        if($connexion ){
+            echo "tu es connecté à la BDD!";
+        }
+        else{echo "Echec de connexion à la BDD";}
+        */
+        //on fait une requete SQL pour insérer l'ensemble des informations de la table utilisateurs
+        if (isset($_POST["login"]) && isset($_POST["password"])&&isset($_POST["confirm_password"]) ){
+        $login=$_POST["login"];
+        $password=$_POST["password"];
+        $confirm_password=$_POST["confirm_password"];
+        }
+        //Si les champs ne sont pas vides, on insère les données dans la base de données
+        if(!empty($login) && !empty($password) &&
+        !empty($_POST["confirm_password"]) && ($_POST["password"]==$_POST["confirm_password"])) {
+            // Vérifie si le login existe déjà
+            $check = "SELECT * FROM utilisateurs WHERE login='$login'";
+            $check_result = mysqli_query($connexion, $check);
+            if(mysqli_num_rows($check_result) > 0) {
+                $message = "Ce login existe déjà, veuillez en choisir un autre.";
+                echo "<p>$message</p>";
+            } else {
+                // Hash du mot de passe
+                $password_hash = password_hash($password, PASSWORD_DEFAULT);
+                $command = "INSERT INTO utilisateurs (id,login,password) VALUES (0,'$login','$password_hash')";
+                $result = mysqli_query($connexion, $command);
+                $message = 'Parfait, ton utilisateur a bien été créé!';
+                echo "<p>$message</p>";
+            }
+}
+            
+       
+
+       
+        ?>
+
+
+        <div class=container_form>
+            <h2>Connexion</h2>
+            <br>
+            <form id=form action="profil.php" method="post">
+                <label><b>Login:</b></label><br>
+                <input type="text" name="login"><br>
+                <label><b>Password:</b></label><br>
+                <input type="text" name="password"><br><br>
+                <input class=bouton_submit type="submit" value="Envoyer">
+
+            </form>
+
+        </div>
+
+
+
+
+    </main>
+
+    <footer>
+
+        <div class="container_footer">
+
+            <div>
+                <h3>Services</h3>
+                <nav>
+                    <a href="#">Web design</a>
+                    <a href="#">Development</a>
+                    <a href="#">Hosting</a>
+                </nav>
+            </div>
+            <div>
+                <h3>About</h3>
+                <nav>
+                    <a href="#">Company</a>
+                    <a href="#">Team</a>
+                    <a href="#">Careers</a>
+                </nav>
+            </div>
+
+            <div>
+                <h3>Company </h3>
+                <nav>
+                    <a href="#">Awards</a>
+                    <a href="#">Method</a>
+                    <a href="#">Contact</a>
+                </nav>
+            </div>
+        </div>
+
+        <hr class=hr_milieu>
+
+        <p> ©All Rights Reserved.</p>
+        </div>
+
+
+    </footer>
+
+
+</body>
+
+</html>
---
+++ livreor.php
@@ -0,0 +1,270 @@
+  <?php
+        session_start();
+        $message = '';      // Message à afficher à l'utilisateur
+        /*****************************************
+         *  Vérification du formulaire de commantaire
+        *****************************************/
+        // Si le tableau $_POST existe alors le formulaire a été envoyé
+        if(!empty($_POST))
+        {   
+           
+               
+            // Le login est-il rempli ?
+            if(empty($_POST['comment']))
+            {
+            $message = "Veuillez indiquer votre commentaire svp avant de l'envoyer !";
+            }
+                
+            
+        }
+
+?>
+
+
+  <!DOCTYPE html>
+  <html lang="fr">
+
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Livre d'or</title>
+      <link rel="stylesheet" href="./assets/css/style.css" />
+
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet">
+
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@100..900&display=swap" rel="stylesheet">
+
+
+      <!-- Load an icon library -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+
+  </head>
+
+  <body>
+
+      <header>
+
+
+          <ul>
+              <li>
+                  <a href="./index.php" class="#">
+                      <div class="icon">
+                          <i class="fa fa-home"></i>
+                          <i class="fa fa-home"></i>
+                      </div>
+                      <div class="name">Home</div>
+                  </a>
+              </li>
+
+              <?php if (!isset($_SESSION['login'])): ?>
+              <li>
+
+                  <a href="./connexion.php" class="#">
+                      <div class="icon">
+                          <i class="fa fa-user-circle-o"></i>
+                          <i class="fa fa-user-circle-o"></i>
+                      </div>
+                      <div class="name">Se connecter</div>
+                  </a>
+
+              </li>
+              <li>
+                  <a href="./inscription.php" class="#">
+                      <div class="icon">
+                          <i class="fa fa-user-plus"></i>
+                          <i class="fa fa-user-plus"></i>
+                      </div>
+                      <div class="name">Inscription</div>
+                  </a>
+              </li>
+              <li>
+                  <a href="./livreor.php" class="active">
+                      <div class="icon">
+                          <i class="fa fa-book"></i>
+                          <i class="fa fa-book"></i>
+                      </div>
+                      <div class="name">Livre d'or</div>
+                  </a>
+              </li>
+
+              <?php else: ?>
+
+              <li>
+                  <a href="./profil.php" class="#">
+                      <div class="icon">
+                          <i class="fa fa-user"></i>
+                          <i class="fa fa-user"></i>
+                      </div>
+                      <div class="name">Profil</div>
+                  </a>
+              </li>
+
+              <li>
+                  <a href="./commentaire.php" class="#">
+                      <div class="icon">
+                          <i class="fa fa-edit"></i>
+                          <i class="fa fa-edit"></i>
+                      </div>
+                      <div class="name">Commentaires</div>
+                  </a>
+              </li>
+              <li>
+                  <a href="./livreor.php" class="active">
+                      <div class="icon">
+                          <i class="fa fa-book"></i>
+                          <i class="fa fa-book"></i>
+                      </div>
+                      <div class="name">Livre d'or</div>
+                  </a>
+              </li>
+              <li>
+                  <a href="./deconnexion.php" class="#">
+                      <div class="icon">
+                          <i class="fa fa-sign-out"></i>
+                          <i class="fa fa-sign-out"></i>
+                      </div>
+                      <div class="name">Deconnexion</div>
+                  </a>
+              </li>
+              <?php endif; ?>
+          </ul>
+
+
+
+
+      </header>
+
+
+
+      <main>
+
+          <?php if(!empty($message)) : ?>
+          <p><?php echo $message; ?></p>
+          <?php endif; ?>
+          <?php
+        //on établit la connexion avec la base de donnée moduleconnexion
+        $connexion = mysqli_connect('localhost', 'root');
+        mysqli_select_db($connexion, 'livreor'); 
+        
+        
+        //on fait une requete SQL pour insérer l'ensemble des informations du commentaire dans la table commentaires
+        if (isset($_POST["comment"]) ){
+        $comment=$_POST["comment"];
+        date_default_timezone_set('Europe/Paris');
+        $date = date('Y-m-d H:i:s'); // Format de date et heure
+        $id_utilisateur= $_SESSION['id'];
+        }
+        //Si les champs ne sont pas vides, on insère les données dans la base de données
+           if(!empty($comment)){
+                // On insère le commentaire dans la base de données
+            $command = "INSERT INTO commentaires  (id,commentaire,id_utilisateur,date) VALUES (0,'$comment','$id_utilisateur','$date')";
+            $result = mysqli_query($connexion, $command);
+            $message = 'Parfait, ton commentaire a bien été créé!';
+            echo "<p>$message</p>";
+        }
+    
+
+
+
+        ?>
+          <div class=container_form2>
+
+              <h2>Livre d'or</h2>
+
+
+
+              <table>
+
+
+                  <tr>
+                      <?php       
+                // Récupérer tous les commentaires, du plus récent au plus ancien
+          $command = "SELECT commentaires.date AS 'Posté le' ,utilisateurs.login AS 'Par utilisateur',commentaires.commentaire AS 'Commentaires'
+          FROM commentaires
+          JOIN utilisateurs ON commentaires.id_utilisateur = utilisateurs.id
+          ORDER BY commentaires.date DESC ";
+          $result = mysqli_query($connexion, $command);
+          $user = mysqli_fetch_assoc($result);     
+                      
+                      
+                      
+                      
+            //on recupère le header de notre table 
+            $fields = mysqli_fetch_fields($result);
+            foreach ($fields as $field) {
+            echo"<th>".htmlspecialchars($field->name)."</th>";}
+            ?>
+                  </tr>
+                  <?php    //on recupère le body de notre table 
+            while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr>";
+            foreach ($fields as $field) {
+            $fieldName = $field->name;
+            echo "<td>" . htmlspecialchars($row[$fieldName]) . "</td>";
+            }
+            echo"</tr>";
+            }
+        
+            ?>
+
+              </table>
+          </div>
+          <?php if (!isset($_SESSION['login'])): ?>
+          <button class="button" onclick="window.location.href='connexion.php'">Connecter vous pour ajouter un
+              commentaire</button>
+          <?php endif; ?>
+
+      </main>
+
+      <footer>
+
+          <div class="container_footer">
+
+              <div>
+                  <h3>Services</h3>
+                  <nav>
+                      <a href="#">Web design</a>
+                      <a href="#">Development</a>
+                      <a href="#">Hosting</a>
+                  </nav>
+              </div>
+              <div>
+                  <h3>About</h3>
+                  <nav>
+                      <a href="#">Company</a>
+                      <a href="#">Team</a>
+                      <a href="#">Careers</a>
+                  </nav>
+              </div>
+
+              <div>
+                  <h3>Company </h3>
+                  <nav>
+                      <a href="#">Awards</a>
+                      <a href="#">Method</a>
+                      <a href="#">Contact</a>
+                  </nav>
+              </div>
+          </div>
+
+          <hr class=hr_milieu>
+
+          <p> ©All Rights Reserved.</p>
+          </div>
+
+
+      </footer>
+
+
+  </body>
+
+  </html>
